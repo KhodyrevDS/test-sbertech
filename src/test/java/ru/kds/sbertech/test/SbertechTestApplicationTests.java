49bfb8ef4807cf16846ec130ed3e2d1c55a053a0
Данил Ходырев <dhodyrev@bis.ru>
2019-07-27 17:43:27 +0700

    init

---
+++ src/test/java/ru/kds/sbertech/test/SbertechTestApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.kds.sbertech.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbertechTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
